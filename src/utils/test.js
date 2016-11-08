a6b921bdbab88f4023ff3625a328584b3ac4ff64
xiaowei.yang <xiaowei.yang@ele.me>
2016-11-08 15:06:50 +0800

    add unique

--- src/utils/test.js
+++ src/utils/test.js
@@ -3,4 +3,13 @@ export default class Array {
     let a = message || 'hello world'
     console.log(a)
   }
+
+  // 数组去重
+  unique (arr) {
+    let result = []
+    arr.map(item => {
+      result.indexOf(item) === -1 ? result.push(item) : null
+    })
+    return result
+  }
 }
