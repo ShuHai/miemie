adcf3bdaf76d1c1f79cfb1b06c6329412ed96237
xiaowei.yang <xiaowei.yang@ele.me>
2017-01-13 17:50:27 +0800

    完善基本结构

--- src/miemie.js
+++ src/miemie.js
@@ -1,11 +1,19 @@
 import ArrayMethod from './utils/array'
+import DateMethod from './utils/date'
+import MathMethod from './utils/math'
 import NumberMethod from './utils/number'
 import ObjectMethod from './utils/object'
+import RegMethod from './utils/reg'
+import StringMethod from './utils/string'
 class Mie {
   constructor () {
     this.array = new ArrayMethod()
+    this.number = new DateMethod()
+    this.object = new MathMethod()
     this.number = new NumberMethod()
     this.object = new ObjectMethod()
+    this.reg = new RegMethod()
+    this.string = new StringMethod()
   }
 }
 
