15fe81fac32ac5fb366938ce84256d922a807612
xiaowei.yang <xiaowei.yang@ele.me>
2017-01-19 21:42:31 +0800

    update string

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,2 +1,14 @@
 export default class StringMethod {
+  isString (str) {
+    return typeof str === 'string'
+  }
+
+  // 首字母大写
+  capitalize (str) {
+    if (this.isString(str)) {
+      let arr = str.split('')
+      arr[0] = arr[0].toUpperCase()
+      return arr.join('')
+    }
+  }
 }
