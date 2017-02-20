5e6fad344c3e13b819238f4741c8411fed9f841f
xiaowei.yang <xiaowei.yang@ele.me>
2017-02-20 20:47:14 +0800

    uodate

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,7 +2,7 @@ export default class DateMethod {
   // 获取所有跟今天有关的信息
   getToday () {
     let today = new Date()
-    var todayObj = {}
+    let todayObj = {}
     todayObj.year = today.getFullYear()
     todayObj.month = today.getMonth() + 1
     todayObj.day = today.getDate()
@@ -12,5 +12,21 @@ export default class DateMethod {
     todayObj.weekend = today.getDay()
     todayObj.tiemStamp = today.getTime()
     return todayObj
+  }
+
+  // 获取跟今天比较的任意一天
+  getDayFromToday (time) {
+    let day = new Date()
+    day.setDate(day.getDate() + time)
+    let dayObj = {}
+    dayObj.year = day.getFullYear()
+    dayObj.month = day.getMonth() + 1
+    dayObj.day = day.getDate()
+    dayObj.hour = day.getHours()
+    dayObj.minute = day.getMinutes()
+    dayObj.second = day.getSeconds()
+    dayObj.weekend = day.getDay()
+    dayObj.tiemStamp = day.getTime()
+    return dayObj
   }
 }
