005ce8d776b6c11bac3b0df901be9353c92b9184
xiaowei.yang <xiaowei.yang@ele.me>
2017-03-04 20:32:07 +0800

    phoneReg

--- src/utils/reg.js
+++ src/utils/reg.js
@@ -5,4 +5,14 @@ export default class RegMethod {
     return reg.test(string)
   }
 
+  // 判断手机号是否合法, true严格， false只检查位数和数字
+  checkPhone (string, isStrict) {
+    if (isStrict) {
+      let reg = new RegExp('^1[34578]\\d{9}$')
+      return reg.test(string)
+    } else {
+      let reg = new RegExp('^\\d{11}$')
+      return reg.test(string)
+    }
+  }
 }
