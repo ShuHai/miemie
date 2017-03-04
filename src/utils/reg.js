b7ac3ba03fa5f4127d0f182bcff1cd2623539055
xiaowei.yang <xiaowei.yang@ele.me>
2017-03-04 20:36:45 +0800

    just check

--- src/utils/reg.js
+++ src/utils/reg.js
@@ -1,18 +1,22 @@
 export default class RegMethod {
+  // 丢给我正则我吐给你结果
+  justCheck (string, rule) {
+    let reg = new RegExp(rule)
+    return reg.test(string)
+  }
   // 判断字符串中是否全是数字
   checkNum (string) {
-    let reg = new RegExp('^[0-9]*$')
-    return reg.test(string)
+    return this.justCheck(string, '^[0-9]*$')
   }
 
   // 判断手机号是否合法, true严格， false只检查位数和数字
   checkPhone (string, isStrict) {
     if (isStrict) {
-      let reg = new RegExp('^1[34578]\\d{9}$')
-      return reg.test(string)
+      return this.justCheck(string, '^1[34578]\\d{9}$')
     } else {
-      let reg = new RegExp('^\\d{11}$')
-      return reg.test(string)
+      return this.justCheck(string, '^\\d{11}$')
     }
   }
+
+
 }
